02b78b5968fe27b67709dc2dd531f87931027fb1
Mohéddine HAMILA <moheddine.hamila@esprit.tn>
2020-08-12 21:39:35 +0100

    first commit

---
+++ src/main/java/HR/Databank/Elbes/ElbesApplication.java
@@ -0,0 +1,13 @@
+package HR.Databank.Elbes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElbesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElbesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/HR/Databank/Elbes/ElbesApplicationTests.java
@@ -0,0 +1,13 @@
+package HR.Databank.Elbes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElbesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
