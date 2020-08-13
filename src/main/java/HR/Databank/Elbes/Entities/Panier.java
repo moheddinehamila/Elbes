14fdff9c448481c8be95195dc135f4e82eaa9d58
sirineaouem <sirineaouem@gmail.com>
2020-08-13 13:25:01 +0200

    fixes #4

---
+++ src/main/java/HR/Databank/Elbes/Entities/Panier.java
@@ -0,0 +1,50 @@
+package HR.Databank.Elbes.Entities;
+
+public class Panier {
+
+    public int UserId;
+    private Long idArticle;
+    private int qte;
+    private float prixtotal;
+
+    public Panier(int userId, Long idArticle, int qte, float prixtotal) {
+        UserId = userId;
+        this.idArticle = idArticle;
+        this.qte = qte;
+        this.prixtotal = prixtotal;
+    }
+    public Panier() {
+    }
+
+    public int getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(int userId) {
+        UserId = userId;
+    }
+
+    public Long getIdArticle() {
+        return idArticle;
+    }
+
+    public void setIdArticle(Long idArticle) {
+        this.idArticle = idArticle;
+    }
+
+    public int getQte() {
+        return qte;
+    }
+
+    public void setQte(int qte) {
+        this.qte = qte;
+    }
+
+    public float getPrixtotal() {
+        return prixtotal;
+    }
+
+    public void setPrixtotal(float prixtotal) {
+        this.prixtotal = prixtotal;
+    }
+}
