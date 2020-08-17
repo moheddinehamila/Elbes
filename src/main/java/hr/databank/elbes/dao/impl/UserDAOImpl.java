3098ac206589cdcfc39461325603776c7fb79f5b
moheddinehamila <moheddine.hamila@esprit.tn>
2020-08-17 11:26:00 +0100

    adding annotations to user

---
+++ src/main/java/hr/databank/elbes/dao/impl/UserDAOImpl.java
@@ -0,0 +1,39 @@
+package hr.databank.elbes.dao.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import hr.databank.elbes.dao.repo.IUserDAO;
+import hr.databank.elbes.entities.User;
+
+public class UserDAOImpl implements IUserDAO{
+
+	@Override
+	public User ajouterProduit(User produit) {
+		return null;
+	}
+
+	@Override
+	public User modifierProduit(User produit) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean supprimerProduit(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<User> findById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
