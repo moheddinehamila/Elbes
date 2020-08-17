f81d3b9837109f43be217b3a6dfc312bc3b59974
sirineaouem <sirineaouem@gmail.com>
2020-08-17 13:25:58 +0200

    updates

--- src/main/java/hr/databank/elbes/entities/Order.java
+++ src/main/java/hr/databank/elbes/entities/Order.java
@@ -1,2 +1,37 @@
-package hr.databank.elbes.entities;public class Order {
+package hr.databank.elbes.entities;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Date;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Order implements Serializable {
+    @EmbeddedId
+    private OrderPK orderPK;
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
