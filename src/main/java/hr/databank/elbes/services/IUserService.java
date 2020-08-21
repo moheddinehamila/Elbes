9debd6f2c4e7b96d2071a2e68ea7bc67e44cf2ed
moheddinehamila <47010681+moheddinehamila@users.noreply.github.com>
2020-08-21 09:56:44 +0100

    Update IUserService.java

--- src/main/java/hr/databank/elbes/services/IUserService.java
+++ src/main/java/hr/databank/elbes/services/IUserService.java
@@ -1,15 +1,15 @@
 package hr.databank.elbes.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import hr.databank.elbes.entities.UserEntity;
 
- 
 public interface IUserService {
 
     UserEntity AddUser(UserEntity u);
     UserEntity UpdateUser(UserEntity u);
     boolean DeleteUser(int id);
-    UserEntity findById(int id);
-    List<UserEntity> getAll();
+    Optional<UserEntity> findById(int id);
+     List<UserEntity> getAll();
 }
