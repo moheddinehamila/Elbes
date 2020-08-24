c96f32511a307f00cdb5b13fdcbbf9fed610caf5
bhyhamdi <belhamdi7@gmail.com>
2020-08-24 10:50:41 +0100

    v2_controler

--- src/main/java/hr/databank/elbes/ElbesApplication.java
+++ src/main/java/hr/databank/elbes/ElbesApplication.java
@@ -1,6 +1,5 @@
 package hr.databank.elbes;
 
-import hr.databank.elbes.controller.AritcleController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/hr/databank/elbes/services/IArticleService.java
+++ src/main/java/hr/databank/elbes/services/IArticleService.java
@@ -3,12 +3,17 @@ package hr.databank.elbes.services;
 import hr.databank.elbes.entities.Article;
 
 import java.util.List;
+
 public interface IArticleService {
-    List<Article> getArticles ();
-    Article getArticle (int articleId);
-    Article createArticle (Article article);
-    Article updateArticle  (int articleId , Article article);
-    boolean deleteArticle(int articleId);
+    List<Article> getArticles();
+
+    Article getArticle(int articleId);
+
+    Article createArticle(Article article);
+
+    Article updateArticle(Article article);
+
+    boolean deleteArticle(Article article);
 
 
 }
