c00d5eece7e6996c9096f864df17c7a292a859e3
hamdi bel hadj youssef <45259633+bhyhamdi@users.noreply.github.com>
2020-08-24 10:52:27 +0100

    Update ArticleDAOImpl.java

--- src/main/java/hr/databank/elbes/dao/impl/ArticleDAOImpl.java
+++ src/main/java/hr/databank/elbes/dao/impl/ArticleDAOImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
+
 import javax.transaction.Transactional;
 import java.util.*;
 
